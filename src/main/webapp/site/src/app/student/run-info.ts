0afa553b0b023f0e0b956f62b6986e10649dfdb9
Hiroki Terashima <honchikun@gmail.com>
2020-05-02 16:09:13 -0700

    Implemented randomly allocating students to periods. #52

---
+++ src/main/webapp/site/src/app/student/run-info.ts
@@ -0,0 +1,11 @@
+export class RunInfo {
+  isRandomPeriodAssignment: boolean = false;
+  periods: string[];
+  runCode: string;
+  runId: number;
+  runTitle: string;
+  startTime: number;
+  teacherName: string;
+  error: string;
+  name: string;
+}
